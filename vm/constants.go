768a093a34e1bec32da32139b7c5a9350aaf1839
Yurii Biurher <yurii.biurher@zorallabs.com>
2026-03-14 11:52:53 +0200

    refactoring

---
+++ vm/constants.go
@@ -0,0 +1,12 @@
+package vm
+
+const (
+	// GlobalsSize is the maximum number of global variables for a VM.
+	GlobalsSize = 1024
+
+	// StackSize is the maximum stack size for a VM.
+	StackSize = 2048
+
+	// MaxFrames is the maximum number of function frames for a VM.
+	MaxFrames = 1024
+)

b5d5ed448ede0ea562e889d41b109d9f37687ad4
Yurii Biurher <yurii.biurher@zorallabs.com>
2026-03-17 13:45:12 +0200

    refactoring

--- stdlib/json.go
+++ stdlib/json.go
@@ -1,31 +1,22 @@
 package stdlib
 
 import (
+	"bytes"
+	gojson "encoding/json"
+
 	"github.com/jokruger/gs/core"
+	gse "github.com/jokruger/gs/error"
+	"github.com/jokruger/gs/stdlib/json"
+	"github.com/jokruger/gs/value"
 )
 
 var jsonModule = map[string]core.Object{
-	/*
-		"decode": &value.BuiltinFunction{
-			Name:  "decode",
-			Value: jsonDecode,
-		},
-		"encode": &value.BuiltinFunction{
-			Name:  "encode",
-			Value: jsonEncode,
-		},
-		"indent": &value.BuiltinFunction{
-			Name:  "encode",
-			Value: jsonIndent,
-		},
-		"html_escape": &value.BuiltinFunction{
-			Name:  "html_escape",
-			Value: jsonHTMLEscape,
-		},
-	*/
+	"decode":      value.NewBuiltinFunction("decode", jsonDecode, 1, false),
+	"encode":      value.NewBuiltinFunction("encode", jsonEncode, 1, false),
+	"indent":      value.NewBuiltinFunction("indent", jsonIndent, 3, false),
+	"html_escape": value.NewBuiltinFunction("html_escape", jsonHTMLEscape, 1, false),
 }
 
-/*
 func jsonDecode(args ...core.Object) (ret core.Object, err error) {
 	if len(args) != 1 {
 		return nil, gse.ErrWrongNumArguments
@@ -33,27 +24,19 @@ func jsonDecode(args ...core.Object) (ret core.Object, err error) {
 
 	switch o := args[0].(type) {
 	case *value.Bytes:
-		v, err := json.Decode(o.Value)
+		v, err := json.Decode(o.Native())
 		if err != nil {
-			return &value.Error{
-				Value: &value.String{Value: err.Error()},
-			}, nil
+			return value.NewError(value.NewString(err.Error())), nil
 		}
 		return v, nil
 	case *value.String:
-		v, err := json.Decode([]byte(o.Value))
+		v, err := json.Decode([]byte(o.Native()))
 		if err != nil {
-			return &value.Error{
-				Value: &value.String{Value: err.Error()},
-			}, nil
+			return value.NewError(value.NewString(err.Error())), nil
 		}
 		return v, nil
 	default:
-		return nil, gse.ErrInvalidArgumentType{
-			Name:     "first",
-			Expected: "bytes/string",
-			Found:    args[0].TypeName(),
-		}
+		return nil, gse.ErrInvalidArgumentType{Name: "first", Expected: "bytes/string", Found: args[0].TypeName()}
 	}
 }
 
@@ -64,10 +47,10 @@ func jsonEncode(args ...core.Object) (ret core.Object, err error) {
 
 	b, err := json.Encode(args[0])
 	if err != nil {
-		return &value.Error{Value: &value.String{Value: err.Error()}}, nil
+		return value.NewError(value.NewString(err.Error())), nil
 	}
 
-	return &value.Bytes{Value: b}, nil
+	return value.NewBytes(b), nil
 }
 
 func jsonIndent(args ...core.Object) (ret core.Object, err error) {
@@ -77,47 +60,31 @@ func jsonIndent(args ...core.Object) (ret core.Object, err error) {
 
 	prefix, ok := args[1].AsString()
 	if !ok {
-		return nil, gse.ErrInvalidArgumentType{
-			Name:     "prefix",
-			Expected: "string(compatible)",
-			Found:    args[1].TypeName(),
-		}
+		return nil, gse.ErrInvalidArgumentType{Name: "prefix", Expected: "string(compatible)", Found: args[1].TypeName()}
 	}
 
 	indent, ok := args[2].AsString()
 	if !ok {
-		return nil, gse.ErrInvalidArgumentType{
-			Name:     "indent",
-			Expected: "string(compatible)",
-			Found:    args[2].TypeName(),
-		}
+		return nil, gse.ErrInvalidArgumentType{Name: "indent", Expected: "string(compatible)", Found: args[2].TypeName()}
 	}
 
 	switch o := args[0].(type) {
 	case *value.Bytes:
 		var dst bytes.Buffer
-		err := gojson.Indent(&dst, o.Value, prefix, indent)
+		err := gojson.Indent(&dst, o.Native(), prefix, indent)
 		if err != nil {
-			return &value.Error{
-				Value: &value.String{Value: err.Error()},
-			}, nil
+			return value.NewError(value.NewString(err.Error())), nil
 		}
-		return &value.Bytes{Value: dst.Bytes()}, nil
+		return value.NewBytes(dst.Bytes()), nil
 	case *value.String:
 		var dst bytes.Buffer
-		err := gojson.Indent(&dst, []byte(o.Value), prefix, indent)
+		err := gojson.Indent(&dst, []byte(o.Native()), prefix, indent)
 		if err != nil {
-			return &value.Error{
-				Value: &value.String{Value: err.Error()},
-			}, nil
+			return value.NewError(value.NewString(err.Error())), nil
 		}
-		return &value.Bytes{Value: dst.Bytes()}, nil
+		return value.NewBytes(dst.Bytes()), nil
 	default:
-		return nil, gse.ErrInvalidArgumentType{
-			Name:     "first",
-			Expected: "bytes/string",
-			Found:    args[0].TypeName(),
-		}
+		return nil, gse.ErrInvalidArgumentType{Name: "first", Expected: "bytes/string", Found: args[0].TypeName()}
 	}
 }
 
@@ -129,18 +96,13 @@ func jsonHTMLEscape(args ...core.Object) (ret core.Object, err error) {
 	switch o := args[0].(type) {
 	case *value.Bytes:
 		var dst bytes.Buffer
-		gojson.HTMLEscape(&dst, o.Value)
-		return &value.Bytes{Value: dst.Bytes()}, nil
+		gojson.HTMLEscape(&dst, o.Native())
+		return value.NewBytes(dst.Bytes()), nil
 	case *value.String:
 		var dst bytes.Buffer
-		gojson.HTMLEscape(&dst, []byte(o.Value))
-		return &value.Bytes{Value: dst.Bytes()}, nil
+		gojson.HTMLEscape(&dst, []byte(o.Native()))
+		return value.NewBytes(dst.Bytes()), nil
 	default:
-		return nil, gse.ErrInvalidArgumentType{
-			Name:     "first",
-			Expected: "bytes/string",
-			Found:    args[0].TypeName(),
-		}
+		return nil, gse.ErrInvalidArgumentType{Name: "first", Expected: "bytes/string", Found: args[0].TypeName()}
 	}
 }
-*/
